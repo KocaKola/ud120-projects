acc02f92573c268393f339bc311b7ddbb3062ad1
KocaKola <KocaKola@users.noreply.github.com>
2018-02-01 13:25:04 +0800

    finished the validation and evaluation part

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -17,7 +17,7 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
-
+sort_keys = '../tools/python2_lesson14_keys.pkl'
 ### add more features to features_list!
 features_list = ["poi", "salary"]
 
@@ -27,5 +27,21 @@ labels, features = targetFeatureSplit(data)
 
 
 ### your code goes here 
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 
+### it's all yours from here forward!
+from sklearn.tree import DecisionTreeClassifier
 
+clf = DecisionTreeClassifier()
+clf.fit(features_train,labels_train)
+pred = list(clf.predict(features_test))
+accu = clf.score(features_test, labels_test)
+print(accu)
+pred = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1]
+labels_test = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+
+from sklearn.metrics import precision_score, recall_score
+precision = precision_score(labels_test, pred)
+recall = recall_score(labels_test, pred)
+print(precision, recall)
--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -16,7 +16,7 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
-
+sort_keys = '../tools/python2_lesson14_keys.pkl'
 ### first element is our labels, any added elements are predictor
 ### features. Keep this the same for the mini-project, but you'll
 ### have a different feature list when you do the final project.
@@ -25,8 +25,13 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
-
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 
 ### it's all yours from here forward!  
+from sklearn.tree import DecisionTreeClassifier
 
-
+clf = DecisionTreeClassifier()
+clf.fit(features_train,labels_train)
+accu = clf.score(features_test, labels_test)
+print(accu)
